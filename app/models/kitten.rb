770f27964e491e55df8ae04a9eed1252688e0a11
Magmusacy <xpfk.098@gmail.com>
2022-06-01 17:39:57 +0200

    Add RESTful API to the app

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,4 +1,8 @@
 class Kitten < ApplicationRecord
   # Ensure kitten's name starts with capital letter
   before_save { |kitten| kitten.name.capitalize! }
+
+  def as_json(options = {})
+    super(except: [:created_at, :updated_at])
+  end
 end
